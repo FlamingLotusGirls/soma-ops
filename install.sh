7e37a3f207cc79305b17d4d4a599791df40acb37
Michael C. Toren <mct@toren.net>
2014-07-14 23:04:44 -0700

    checkpoint

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ set -e
 set -x
 
 install -p -o root -g root -m 755 bin/init.d-ubrain-clock	/etc/init.d/ubrain-clock
+install -p -o root -g root -m 755 bin/soma-scheduler		/usr/local/bin
+install -p -o root -g root -m 755 bin/ubrain-daemon		/usr/local/bin
 install -p -o root -g root -m 755 bin/ubrain-get-time		/usr/local/bin
 
 update-rc.d ubrain-clock defaults 1
